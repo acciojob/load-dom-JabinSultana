ada8f0496b902a0c6e5fe7662533d8936520a714
JabinSultana <jabinasiff9926@gmail.com>
2026-03-23 21:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+// Wait for the DOM to fully load
+document.addEventListener("DOMContentLoaded", function() {
+  // Write the string to the body
+  document.body.textContent = "DOM load success";
+});
